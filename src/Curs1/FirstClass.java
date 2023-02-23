c77f5d2fa1354b7ddafb74bab89328f270524110
Diana Radu <dianaradu06@yahoo.com>
2023-02-23 21:46:42 +0200

    first commit

--- src/Curs1/FirstClass.java
+++ src/Curs1/FirstClass.java
@@ -1,11 +1,21 @@
 package Curs1;
 
 public class FirstClass {
-
+	// variabila de instanta -> are nevoie de un obiect al clasei pt a fi utilizata 
+	String nume = "Ioana";
+	int varsta = 25;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// metoda main- este metoda principala de executie
 		System.out.println("Test");
-
+		
+		FirstClass obiect = new FirstClass();
+		
+		// este o variabila locala
+		String prenume = "Ana";
+		System.out.println(prenume);
+		
+		System.out.println(obiect.nume);
 	}
 
 }
