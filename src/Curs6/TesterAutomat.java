9064561934ffca2dff30242f942677f452c00dd6
Diana Radu <dianaradu06@yahoo.com>
2023-03-13 18:43:20 +0200

    curs6

---
+++ src/Curs6/TesterAutomat.java
@@ -0,0 +1,13 @@
+package Curs6;
+
+public class TesterAutomat extends Tester{
+	public String programimgLang;
+
+	public String getProgramimgLang() {
+		return programimgLang;
+	}
+
+	public void setProgramimgLang(String programimgLang) {
+		this.programimgLang = programimgLang;
+	}
+}
